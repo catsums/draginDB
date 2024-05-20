d8da01930bae34c95ac2a309d56eb85b1870f985
Catsim <111745304+catsums@users.noreply.github.com>
2024-05-20 22:19:02 +0200

    Test main

---
+++ main.ts
@@ -0,0 +1,22 @@
+
+import * as MY from "@catsums/my";
+import _ from "lodash";
+import { DBDatabase } from "./database";
+
+async function main(){
+
+	// let db = await DBDatabase.ReadDatabase("./db/myDB.json");
+
+	// let pack = db.getPackByName("users");
+
+	// let record = pack.createRecord({
+	// 	id: "ABC124",
+	// 	name: "John",
+	// 	alive: true,
+	// 	age: 21,
+	// });
+
+	// DBDatabase.WriteDatabase(db);
+}
+
+main();
